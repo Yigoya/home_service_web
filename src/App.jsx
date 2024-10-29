b8e41b46134b17b53e62387fea7bce6d37cc89f5
yohanamtesfaye <yohanatesfaye24@gmail.com>
2024-10-29 15:38:34 -0400

    Initial Set Up

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Landing from './Shared/pages/Landing'
+import NavBar from './Shared/Components/NavBar'
+import Footer from './Shared/Components/Footer'
+import SignUp from './Customer/Pages/SignUp'
+import Login from './Shared/pages/Login'
+import TechnicianList from './Customer/Pages/TechnicianList'
+import ContactUs from './Customer/Pages/ContactUs'
+import Registration from './Technician/Pages/Registration'
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path='/customer-signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/technician-list' element={<TechnicianList/>} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/technician-registration' element={<Registration />} />
+        </Routes>
+        <Footer />
+      </Router>
+    
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/Customer/Pages/TechnicianDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TechnicianDetail = () => {
+  return (
+    <div>TechnicianDetail</div>
+  )
+}
+
+export default TechnicianDetail
